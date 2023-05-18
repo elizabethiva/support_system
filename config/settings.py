93b0a70a8700c7498223ce94605b74e17aff2e5c
Yelizaveta <lizavetamania@gmail.com>
2023-05-18 19:41:11 +0300

    update

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o2pv@*9pbe#u4)udf0)k8beha$jx3orzda0(-=ox$xvhj&@ezk"  # noqa: E501
+SECRET_KEY = (
+    "django-insecure-o2pv@*9pbe#u4)udf0)k8beha$jx3orzda0(-=ox$xvhj&@ezk"  # noqa: E501
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,17 +88,17 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  
-    }, # noqa: E501
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa: E501
+    },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",  
-    }, # noqa: E501
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",  # noqa: E501
+    },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",  
-    }, # noqa: E501
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",  # noqa: E501
+    },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",  
-    }, # noqa: E501
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",  # noqa: E501
+    },
 ]
 
 
