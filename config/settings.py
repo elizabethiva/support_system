0fdacdfd49141be56c4a9b5004bbde9052237e11
Yelizaveta <lizavetamania@gmail.com>
2023-06-11 12:16:11 +0300

    ➕ Add AUTH_USER_MODEL

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -124,3 +123,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/pokemon"
+
+AUTH_USER_MODEL = "core.User"
