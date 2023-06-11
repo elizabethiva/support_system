a6bf490a71c3fd5ffcee8cf2a735a1a80b6d2a4a
Yelizaveta <lizavetamania@gmail.com>
2023-06-11 12:21:26 +0300

    ✅ Create serializer

---
+++ core/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from core.errors import SerializerError
+
+User = get_user_model()
+
+
+class UserCreateRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+
+
+class UserCreateResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "first_name", "last_name"]
+
+
+class LoginRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+
+class LoginResponseSerializer(serializers.ModelSerializer):
+    """
+    This is pseudocode
+    that reflects the assumed working principle of the serializer.
+    """
+
+    def post(self, request):
+        try_login = LoginRequestSerializer(data=request.data)
+        is_valid = try_login.is_valid()
+        if not is_valid:
+            raise SerializerError(try_login)
+
+        return "Login successful"
