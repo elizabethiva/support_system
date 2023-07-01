8cc2a2aebe188dcafadde34c310f56cc6466e33b
Yelizaveta <lizavetamania@gmail.com>
2023-07-01 08:41:39 +0300

    ⬇️ Swagger documentation is reachable

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView
-)
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from authentication.serializer import LoginResponseSerializer
 
